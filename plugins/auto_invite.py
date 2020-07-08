0c0a07ddf9740dd9d521fb6b7499747cd1cb41ff
Lei Shi <me@leishi.io>
2020-07-08 11:29:26 -0700

    Update auto invite, now send codes from others as well

--- plugins/auto_invite.py
+++ plugins/auto_invite.py
@@ -1,4 +1,4 @@
-import os
+import re
 import pickle
 import random
 import traceback
@@ -8,49 +8,10 @@ from telethon.sync import events
 from plugins.base import Telegram, PluginMount
 from utils import get_url
 
-chats = ["gua_mei_debug"]
-words = [
-    "邀请码来一个",
-    "邀请码走一个",
-    "来个邀请码",
-    "来一个邀请码",
-    "邀请码给个",
-    "有邀请吗",
-    "有邀请码吗",
-    "有人邀请吗",
-    "给个邀请码",
-    "邀请下",
-    "有人邀请码",
-    "邀请有吗",
-    "邀请码多少",
-    "求个邀请码",
-    "求邀请码",
-    "给个邀请码",
-    "有没有邀请码",
-]
-replies = [
-    "邀请码：`AFHj`",
-    "邀请：`AFHj`",
-    "https://www.clashcloud.net/auth/register?code=AFHj",
-    "可以用我的：AFHj" "邀请链接：https://www.clashcloud.net/auth/register?code=AFHj",
-    "可以用我的：`AFHj`",
-    "直接用：`AFHj`",
-    "[点击邀请注册](https://www.clashcloud.net/auth/register?code=AFHj)",
-]
-funny_replies = ["别搞我", "大哥别发了，不会回的", "没用没用没用没用", "没有滚蛋"]
-blocklist = ['qaqwsw']
-
 
 class Action(Telegram, metaclass=PluginMount):
     command_name = "auto_invite"
     prefix = "auto_invite_"
-
-    async def load_vals(self):
-        await self.redis.sadd(f'{self.prefix}chats', chats[0], *chats)
-        await self.redis.sadd(f'{self.prefix}words', words[0], *words)
-        await self.redis.sadd(f'{self.prefix}replies', replies[0], *replies)
-        await self.redis.sadd(f'{self.prefix}funny_replies', funny_replies[0], *funny_replies)
-        await self.redis.sadd(f'{self.prefix}blocklist', blocklist[0], *blocklist)
 
     async def is_blocked(self, sender):
         if sender.id and await self.redis.sismember(f'{self.prefix}blocklist', sender.id):
@@ -66,44 +27,64 @@ class Action(Telegram, metaclass=PluginMount):
 
     async def get_one_reply(self, key):
         return (await self.redis.srandmember(f'{self.prefix}{key}', 1, encoding='utf-8'))[0]
+    
+    async def get_reply(self):
+        codes = await self.redis.hgetall(f'{self.prefix}codes', encoding='utf-8')
+        codes = {k: sum(int(_v) for _v in codes.values()) - int(v) for k,v in codes.items()}
+        return "也可以从群友的这些码里选择：\n{}".format(', '.join(
+            map(lambda c: f'`{c}`', random.choices(list(codes.keys()), list(codes.values()), k=5))))
 
-    async def is_asking_code(self, event):
-        msg = event.message
-        if not msg.is_reply and any([val.decode('utf-8') in event.raw_text.lower() async for val in self.redis.isscan(f'{self.prefix}words')]):
-            to_chat = await event.get_chat()
+    async def not_code(self, code):
+        return all('a' <= c <= 'z' for c in code) or all('A' <= c <= 'Z' for c in code) or all('0' <= c <= '9' for c in code) or await self.redis.sismember(f'{self.prefix}not_codes', code)
+
+    async def is_asking_code(self, msg, to_chat, sender):
+        if not msg.is_reply and msg.text and any([val.decode('utf-8') in msg.text.lower() async for val in self.redis.isscan(f'{self.prefix}words')]):
             if (to_chat.username and await self.redis.sismember(f'{self.prefix}chats', to_chat.username)) \
               or (to_chat.id and await self.redis.sismember(f'{self.prefix}chats', to_chat.id)):
-                sender = await event.get_sender()
                 self._log_message(msg, to_chat, sender)
-                return True, sender
-        return False, None
+                return True
+        return False
+
+    async def is_in_chat(self, to_chat):
+        return (to_chat.username and await self.redis.sismember(f'{self.prefix}chats', to_chat.username)) \
+              or (to_chat.id and await self.redis.sismember(f'{self.prefix}chats', to_chat.id))
 
     def __call__(self, redis):
         import aioredis
+        regex = re.compile(r'^([a-zA-Z0-9]{4})$|^.*邀请码.*([a-zA-Z0-9]{4}).*$|^.*code=([a-zA-Z0-9]{4}).*$', re.MULTILINE)
         @self._client.on(events.NewMessage)
         async def _inner(evt):
             msg = evt.message
             try:
-                is_asking_code, sender = await self.is_asking_code(evt)
-                if is_asking_code:
+                to_chat = await evt.get_chat()
+                if not await self.is_in_chat(to_chat):
+                    return
+
+                sender = await evt.get_sender()
+                m = regex.match(msg.text)
+                if msg.text and m:
+                    for code in m.groups():
+                        if code and not await self.not_code(code):
+                            self._log_message(msg, to_chat, sender)
+                            self._logger.info(f'{telethon_utils.get_display_name(sender)}({sender.id}) sent a code: {code}')
+                            await self.redis.hincrby('auto_invite_codes', code)
+                    return
+                if await self.is_asking_code(msg, to_chat, sender):
                     attempts = await self.incr_attempts(sender)
                     if not await self.is_blocked(sender):
                         if attempts == 1:
-                            reply = await self.get_one_reply('replies')
-                            await msg.reply(reply)
-                        elif attempts == 3:
-                            reply = await self.get_one_reply('funny_replies')
-                            await msg.reply(reply)
+                            one_reply = await self.get_one_reply('replies')
+                            reply = await self.get_reply()
+                            await msg.reply(one_reply+'\n'+reply)
                     else:
-                        self._logger.info(f'{telethon_utils.get_display_name(sender)} in blocklist sent {msg.text}')
-                    if attempts >= 3:
+                        self._logger.info(f'{telethon_utils.get_display_name(sender)}({sender.id}) in blocklist sent {msg.text}')
+                    if attempts >= 3 and sender.id != 419540347:
                         await self.redis.sadd(f'{self.prefix}blocklist', sender.id)
             except Exception as e:
                 traceback.print_exc()
         
         async def connect_redis():
             self.redis = await aioredis.create_redis_pool(f'redis://{redis}')
-            await self.load_vals()
         
         with self._client:
             self._client.loop.run_until_complete(connect_redis())

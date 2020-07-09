79d8fdbe673fcfe7202973233b43359f3ac5491a
Lei Shi <me@leishi.io>
2020-07-08 23:56:46 -0700

    Update word cloud inchat mode.

--- plugins/word_cloud_inchat.py
+++ plugins/word_cloud_inchat.py
@@ -1,4 +1,5 @@
 import re
+import math
 import asyncio
 import traceback
 from io import BytesIO
@@ -17,7 +18,7 @@ class Action(Telegram, metaclass=PluginMount):
     command_name = "word_cloud_inchat"
     prefix = "word_cloud_inchat_"
 
-    async def _generate_word_cloud_async(self, msg_id: str, reply_msg, chat, user, start: datetime, end: datetime):
+    async def _generate_word_cloud_async(self, msg_id: str, reply_msg, to_chat, search_chat, user, start: datetime, end: datetime):
         try:
             import jieba
             from wordcloud import WordCloud
@@ -27,35 +28,39 @@ class Action(Telegram, metaclass=PluginMount):
         words = []
         count = 0
         initial_msg = reply_msg.text + '\n'
-        async for msg in self._client.iter_messages(chat, from_user=user, offset_date=end):
+        async for msg in self._client.iter_messages(search_chat, from_user=user, offset_date=end):
             if start and msg.date < start:
                 break
             if msg.text:
                 words += [w for w in jieba.cut(msg.text) if not await self.redis.sismember(f'{self.prefix}stop_words', w)]
                 count += 1
-            if count % 1000 == 1:
-                initial_msg += '.'
-                await reply_msg.edit(text=initial_msg)
+            if count >= 1000:
+                p = math.floor(math.log(count, 10))
+                if count % int(math.pow(10, p)) == 0:
+                    try:
+                        await reply_msg.edit(text=initial_msg + '.' * (count // 1000))
+                    except Exception as _:
+                        traceback.print_exc()
         try:
             image = WordCloud(font_path="simsun.ttf", width=800, height=400).generate(' '.join(words)).to_image()
             stream = BytesIO()
             image.save(stream, 'PNG')
+            await reply_msg.delete()
             wordcloud_msg = await self._client.send_message(
-                chat,
-                'Wordcloud for\n{}{}{}'.format(
-                    f'{chat.title}',
+                to_chat,
+                '词云 for\n{}{}{}'.format(
+                    f'{search_chat.title}',
                     f'\n{utils.get_display_name(user)}' if user else '',
                     '\n{}-{}'.format(
                         start.strftime('%Y/%m/%d') if start else 'Join',
                         end.strftime('%Y/%m/%d') if end else 'Now') if start or end else ''),
                 reply_to=msg_id,
                 file=stream.getvalue())
-            await asyncio.sleep(3600)
-            await wordcloud_msg.delete()
         except Exception as _:
             traceback.print_exc()
         finally:
-            await reply_msg.delete()
+            await asyncio.sleep(3600)
+            await wordcloud_msg.delete()
             
 
     def __call__(self, redis):
@@ -65,7 +70,12 @@ class Action(Telegram, metaclass=PluginMount):
             msg = event.message
             try:
                 if msg.text and msg.text.lower().startswith('wordcloud'):
-                    chat = await event.get_chat()
+                    to_chat = await event.get_chat()
+                    search_chat = to_chat
+                    m = re.search(r'chat=(?P<chat>[0-9a-zA-Z_\-]+)', msg.text)
+                    if m is not None:
+                        search_chat = await self._client.get_entity(m.groupdict().get('chat'))
+
                     user = None
                     if msg.is_reply:
                         reply_to = await msg.get_reply_message()
@@ -83,14 +93,14 @@ class Action(Telegram, metaclass=PluginMount):
                     else:
                         start = end - timedelta(days=1)
                     reply_words = 'Received wordcloud request for {}{}{}'.format(
-                        f'\n{chat.title}',
+                        f'\n{search_chat.title}',
                         f'\n{utils.get_display_name(user)}' if user else '',
                         '\n`{}-{}`'.format(
                             start.strftime('%Y/%m/%d') if start else 'Join',
                             end.strftime('%Y/%m/%d') if end else 'Now') if start or end else '')
                     self._logger.info(reply_words.replace('\n', ' '))
-                    reply_msg = await self._client.send_message(chat, reply_words, reply_to=msg.id)
-                    await self._generate_word_cloud_async(msg.id, reply_msg, chat, user, start, end)
+                    reply_msg = await self._client.send_message(to_chat, reply_words, reply_to=msg.id)
+                    await self._generate_word_cloud_async(msg.id, reply_msg, to_chat, search_chat, user, start, end)
             except Exception as e:
                 traceback.print_exc()
 
